a5fc65b896ed652270810c4568601aff26423082
Selva A <selva1192023@gmail.com>
2026-06-12 15:16:40 +0530

    Implement score prediction using linear regression
    Add linear regression model to predict student scores based on study hours.

--- Day 3 student_score_prediction.py
+++ Day 3 student_score_prediction.py
@@ -1,1 +1,13 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
+hours = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
+scores = np.array([20, 40, 50, 70, 90])
+
+model = LinearRegression()
+model.fit(hours, scores)
+
+prediction = model.predict([[6]])
+
+print("Predicted Score for 6 Hours Study:")
+print(prediction[0])
